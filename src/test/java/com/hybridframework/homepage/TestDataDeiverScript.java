5f2e1614ff165c4153e02035970d0056a1efa06d
user <user@DESKTOP-5EESGLJ>
2017-11-10 20:38:48 +0530

    initial commit

---
+++ src/test/java/com/hybridframework/homepage/TestDataDeiverScript.java
@@ -0,0 +1,27 @@
+package com.hybridframework.homepage;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.hybridframework.testBase.TestBase;
+
+public class TestDataDeiverScript extends TestBase {
+	
+	@DataProvider(name="testData")
+	public Object[][] dataSource(){
+		return getData("TestData.xlsx", "LoginTestData");
+	}
+	
+	@Test(dataProvider="testData")
+	public void testLogin(String userName, String password, String runmode){
+		System.out.println("userName:-"+userName);
+		System.out.println("password:-"+password);
+		System.out.println("runmode:-"+runmode);
+		//driver.findElement(By.xpath("")).sendKeys(userName);
+		//driver.findElement(By.xpath("")).sendKeys(password);
+		//driver.findElement(By.xpath("")).sendKeys(runmode);
+	}
+
+	
+
+}
